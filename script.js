fd4f40535e501d3d8f54b29a071da8477249e584
Danny DOC <59356195+DanielCodez-py@users.noreply.github.com>
2022-02-17 21:01:17 +0100

    2nd commit

--- script.js
+++ script.js
@@ -17,12 +17,14 @@ btn2.addEventListener("click", function() {player_1.value = "o"; player_2.value 
 var player_1 = {
     moved_cells: [],
     value: "",
+    name: 'Player 1',
     move_pattern: /[13579]/
 }
 
 var player_2 = {
     moved_cells: [], 
     value: "",
+    name: 'Player 2',
     move_pattern: /[2468]/
 }
 
@@ -32,21 +34,30 @@ var universe  = {
     moves :  [],
     p1_val: player_1.value,
     p2_val: player_2.value,
+    restartGame: function re_game(){
+        for (var i in cells){cells[i].innerText = ""; cells[i].className = "blank";}
+        val = ""; 
+        player_1.value = "";
+        player_2.value = "";
+        universe.move_no = 0;
+        universe.moves = [];
+        player_1.moved_cells = [];
+        player_2.moved_cells = [];
+    },
 
 };
 
 // Player move engine
 var val;
 for(let i = 0; i < cellsLength; i++ ){
-    // if (){
-
-    // }
     const cell = cells[i]
+
     // Reflect the number of moves and specific move number in the universe object when a cell is clicked
     cell.addEventListener("click",
         function() {
             universe.move_no ++;
             universe.moves.push(i);
+
             // Simplify a long expression to a variable
             var x = universe.move_no;
 
@@ -54,42 +65,48 @@ for(let i = 0; i < cellsLength; i++ ){
             if (x % 2 != 0){
                 val = player_1.value;
                 cell.innerText = val;
+
                 // Double-click and double- value fix 
                 if (cell.innerText = val) {
                     cell.setAttribute("class", "filled");
                     player_1.moved_cells.push(i)
+                    // Check if a player has fulfilled the winning cell sequence
+                    verifyWin(player_1)
                 }
-                // player_1.moves.push(Number(x));
             }
+
             else if(x % 2 == 0){
                 val = player_2.value;
                 cell.innerText = val;
+                
                 // Double-click and double- value fix 
                 if (cell.innerText = val) {
                     cell.setAttribute("class", "filled");
-                    player_2.moved_cells.push(i); 
+                    player_2.moved_cells.push(i);
+                    // Check if a player has fulfilled the winning cell sequence
+                    verifyWin(player_2)
                 }
-                // player_2.moves.push(Number(x));
             }
-            
-
-
-            // Check if a player has fulfilled the winning cell sequence
-
         }
     )
 }
 
 //Restart Game button function
-resetBtn.addEventListener('click', function(){
-    for (var i in cells){cells[i].innerText = ""; cells[i].className = "blank";}
-    val = ""; 
-    player_1.value = "";
-    player_2.value = "";
-    universe.move_no = 0;
-    universe.moves = [];
-    player_1.moved_cells = [];
-    player_2.moved_cells = [];
-})
+resetBtn.addEventListener('click', universe.restartGame)
 
+
+function verifyWin(player) {
+    a = player.moved_cells.sort()
+
+    p1 = a.map(function(a,b,c) {
+        return a-c[b-1]; 
+    })
+
+    p1.splice(0, 1)
+
+    if (p1[0] && (p1[0] === p1[1])) {
+        alert(`${player.name} won`);
+        universe.restartGame
+    }
+}
 
